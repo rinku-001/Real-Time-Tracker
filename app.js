e02ebfd0f4af1f723ef2c8bfedc371872f6415c4
rinku-001 <rinkushakya2360@gmail.com>
2026-05-14 04:39:38 +0530

    Prepare app for deploy

--- app.js
+++ app.js
@@ -27,4 +27,5 @@ app.get("/", function (req, res) {
     res.render("index");
 });
 
-server.listen(3000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log('Server listening on', PORT));
